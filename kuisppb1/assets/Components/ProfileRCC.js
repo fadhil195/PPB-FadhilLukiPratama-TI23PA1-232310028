e09dc15bbe27a76727846805fe80e835810b1723
fadhil195 <fadhil.luki@gmail.com>
2025-04-18 14:56:09 +0700

    kuis1-ppb

---
+++ kuisppb1/assets/Components/ProfileRCC.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+
+class ProfileRCC extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: 'https://img.lovepik.com/png/20231122/persona-profile-picture-icon-icon-line-icon-for-web-application_666456_wh1200.png' }} 
+          style={styles.avatar}
+        />
+        <Text style={styles.text}>Nama: Fadhil Luki Pratama</Text>
+        <Text style={styles.text}>NPM: 232310028</Text>
+        <Text style={styles.text}>Jurusan: Teknologi Informasi</Text>
+        <Text style={styles.text}>Hobi: Baca Manhwa</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+    padding: 20,
+    backgroundColor: '#fce4ec',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 15,
+  },
+  text: {
+    fontSize: 16,
+    marginVertical: 2,
+  },
+});
+
+export default ProfileRCC;
